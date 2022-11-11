8e2f4ee2cfe537c72d1b0211a1833e0f4560efd6
Jakub Szwajka <szwajkajakub@gmail.com>
2022-11-11 21:56:45 +0100

    Git and alias update

---
+++ zsh/aliases.zsh
@@ -0,0 +1,9 @@
+alias wo='workon "${PWD##*/}"'
+alias ls='exa --long --icons --group-directories-first'
+alias dsc='docker container stop $(docker ps -aq)'
+
+alias s='source ~/.zshrc'
+
+# alias git lg1 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+# alias git lg2 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+# alias git lg3 = "git log --all --decorate --oneline --graph  --format=format:'%C(bold green)%h%C(reset) | %C(white)%s%C(reset) | %C(dim white)- %an%C(reset) %C(dim white)(%ar)%C(reset) | %C(bold yellow)%d%C(reset)'"
