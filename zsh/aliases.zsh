f54452f00a7b56b21f308c2651d1682180772087
JakubSzwajka <szwajkajakub@gmail.com>
2022-11-17 14:42:39 +0100

    Aliases update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,17 @@
 alias wo='workon "${PWD##*/}"'
 alias ls='exa --long --icons --group-directories-first'
+alias l='exa --long --icons --group-directories-first'
 alias dsc='docker container stop $(docker ps -aq)'
+alias cat='bat'
 
 alias s='source ~/.zshrc'
+
+
+# -- git --
+alias gs='git status'
+alias gd='git diff'
+alias gc='git commit'
+alias gdc='bat --diff'
 
 # -- work aliases --- 
 alias glc='mypy . && flake8 . && globality-black . --diff'
